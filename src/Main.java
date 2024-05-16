2fdc95df7ce802400e6a40ad1ce5db46fc63fa69
Naiyal <92427035+Naiyal@users.noreply.github.com>
2024-05-16 18:22:39 +0300

    new2

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
-public class Main {
-    public static void main(String[] args)  ArrayList{
+public class Main extends ArrayList {
+    public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(15, 20, 25));
         ArrayList<Integer> list2 = new ArrayList<>();
+        Main arr = new Main();
         list2 = (ArrayList) list.clone();
         // System.out.println(list.contains(5));
         // for (int i = 0; i < list.size(); i++) {
         // System.out.println(list.get(i));
         // }
+
+        arr.add(10);
         list2.set(0, 20);
         list2.remove(Integer.valueOf(25));
-        System.out.println(list2);
 
     }
 }
