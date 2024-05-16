909f486d391109f9aaa2f7c11c200a222e63c1f6
Naiyal <92427035+Naiyal@users.noreply.github.com>
2024-05-16 20:35:49 +0300

    limda exprition

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
-public class Main extends ArrayList {
+public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(15, 20, 25));
-        ArrayList<Integer> list2 = new ArrayList<>();
-        Main arr = new Main();
-        list2 = (ArrayList) list.clone();
-        // System.out.println(list.contains(5));
-        // for (int i = 0; i < list.size(); i++) {
-        // System.out.println(list.get(i));
-        // }
-
-        arr.add(10);
-        list2.set(0, 20);
-        list2.remove(Integer.valueOf(25));
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(15, 20, 25, 45, 96, 78));
+        list.removeIf(n -> n > 25);
+        System.out.println(list);
 
     }
 }
